411231ff8ed0c38b7ccc6b9174051650c67aead9
S. Chris Colbert <sccolbert@gmail.com>
2015-09-05 23:17:20 -0400

    export module contents

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
 'use strict';
+
+export * from './menu';
+export * from './menuitem';
